ea7771e5b55f93e70562db8e61bb2d6db205d5c5
saddamBD <saddam.ruet@gmail.com>
2024-10-21 11:58:16 +0600

    mongodb connection

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const mongoose = require("mongoose");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+async function main() {
+  await mongoose.connect(process.env.DB_URL);
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
+}
+
+main()
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
